b42af0b18f19ca2ef343b1e73b57cb81c12545d8
Alex Al <alexsputnik@gmail.com>
2022-05-17 11:42:00 +0300

    the first commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,16 @@
 package reverse_string
 
+/*
+ReverseString() - function, that reverses the input string
+the string may have international symbols,emojis and consist of several lines
+*/
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runeSlice := []rune(input) // converting string to []rune
+	revSlice := make([]rune, len(runeSlice))
+	for i, v := range runeSlice {
+		revSlice[len(runeSlice)-1-i] = v
+	}
+	output = string(revSlice)
 	return output
 }
